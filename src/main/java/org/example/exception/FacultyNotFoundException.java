186bc874861a7d64473e9ee8bbdea90cafeccc10
Dmitriy1604 <Dimon312-91@mail.ru>
2022-12-20 18:51:12 +0300

    выполнил

---
+++ src/main/java/org/example/exception/FacultyNotFoundException.java
@@ -0,0 +1,13 @@
+package org.example.exception;
+
+public class FacultyNotFoundException {
+    public FacultyNotFoundException exception RuntimeException {
+
+    }
+        private final long id;
+
+        public FacultyNotFoundException(long id{ this.id = id;}
+
+        public long getId(){return id;}
+    }
+
